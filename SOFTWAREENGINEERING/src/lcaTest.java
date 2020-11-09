5b8283c17499e9330b6fef9c2eab7078a81bba3d
Rosie Connolly <60625969+connolr3@users.noreply.github.com>
2020-11-09 11:53:44 +0000

    DAG tests in LCA.java
    DAG tests added to LCA.java, failed as the current LCA.java needs to be updated to work for  a DAG

--- SOFTWAREENGINEERING/src/lcaTest.java
+++ SOFTWAREENGINEERING/src/lcaTest.java
@@ -39,5 +39,25 @@ class lcaTest {
 		tree.root.left = new Node(2); 
 		assertEquals(2,tree.findLCA(2,2),"LCA of same node");
 	}
+	@Test
+	void testLCAwithDAG() {
+		//The tree being created can be visualised as;
+		//https://algorithms.tutorialhorizon.com/topological-sort/
+		lca tree = new lca(); 
+		tree.root = new Node(7); 
+		tree.root.left = new Node(5); 
+		tree.root.right = new Node(6); 
+		tree.root.right.right = new Node(3); 
+		tree.root.right.left = new Node(4);
+		tree.root.left.right=new Node(4);
+		tree.root.left.left=new Node(2);
+		tree.root.left.left.right=new Node(1);
+		tree.root.right.right.left = new Node(1); 
+		tree.root.right.right.left=new Node(0);
+		assertEquals(7,tree.findLCA(5,6),"LCA of 5 and 6");
+		assertEquals(7,tree.findLCA(2,3),"LCA of 2 and 3");
+		assertEquals(5,tree.findLCA(4,0),"LCA of 5 and 0");
+	}
+
 
 }
