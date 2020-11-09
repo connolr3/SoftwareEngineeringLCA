79514bb6979362b4fae26eeb11c7dfc3bd0c8bec
Rosie Connolly <60625969+connolr3@users.noreply.github.com>
2020-11-09 15:32:56 +0000

    Initial tests to include DAG
    Added a basic test to make sure LCA worked for DAG
    Now need to add more testing as I only tested base case

--- SOFTWAREENGINEERING/src/lcaTest.java
+++ SOFTWAREENGINEERING/src/lcaTest.java
@@ -43,21 +43,22 @@ class lcaTest {
 	void testLCAwithDAG() {
 		//The tree being created can be visualised as;
 		//https://algorithms.tutorialhorizon.com/topological-sort/
+		//DAG dagtree =new DAG(3);
+		DAG dagtree =new DAG(8);
+		dagtree.addEdge(7, 5);
+		dagtree.addEdge(7, 6);
+		dagtree.addEdge(5, 4);
+		dagtree.addEdge(6, 4);
+		dagtree.addEdge(5, 2);
+		dagtree.addEdge(2, 1);
+		dagtree.addEdge(6, 3);
+		dagtree.addEdge(3, 1);
+		dagtree.addEdge(1, 0);
 		lca tree = new lca(); 
-		tree.root = new Node(7); 
-		tree.root.left = new Node(5); 
-		tree.root.right = new Node(6); 
-		tree.root.right.right = new Node(3); 
-		tree.root.right.left = new Node(4);
-		tree.root.left.right=new Node(4);
-		tree.root.left.left=new Node(2);
-		tree.root.left.left.right=new Node(1);
-		tree.root.right.right.left = new Node(1); 
-		tree.root.right.right.left=new Node(0);
-		assertEquals(7,tree.findLCA(5,6),"LCA of 5 and 6");
-		assertEquals(7,tree.findLCA(2,3),"LCA of 2 and 3");
-		assertEquals(5,tree.findLCA(4,0),"LCA of 5 and 0");
+		assertEquals(7,dagtree.findLCA(5,6),"LCA of 7 and 7");
+		assertEquals(7,dagtree.findLCA(2,3),"LCA of 2 and 3");
+		assertEquals(6,dagtree.findLCA(4,0),"LCA of 5 and 0");
+		
 	}
-
 
 }
